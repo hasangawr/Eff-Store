4765ce238bed33368a041ae1c565fccbc631dea8
hasangawr <hasangawr@gmail.com>
2022-12-13 05:00:14 +0530

    Create header and footer
    Bootstrap theme changed to 'Cosmo'

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
   return (
-    <header>header</header>
+    <header>
+        <Navbar bg="success" variant='dark' expand="lg" collapseOnSelect >
+        <Container>
+        <Navbar.Brand href="/">SuperShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" style={{justifyContent:'flex-end'}}>
+          <Nav className="ml-auto" >
+            <Nav.Link href="/cart"><i className='fas fa-shopping-cart' style={{padding: '5px'}}></i>Cart</Nav.Link>
+            <Nav.Link href="/login"><i className='fas fa-user' style={{padding: '5px'}}></i>Sign In</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    </header>
   )
 }
 
