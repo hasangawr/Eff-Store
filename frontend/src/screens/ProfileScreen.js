26fe0908faa88a4b0ce494025d85edd2c157432b
hasangawr <hasangawr@gmail.com>
2023-03-16 08:29:48 +0530

    Revert "Rev"
    This reverts commit 46ccbbd9576c30e1aaf9df2b609f50d041f3cce6.

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Form, Button, Row, Col, Container, Table } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import { Form, Button, Row, Col, Container } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
-import { listMyOrders } from '../actions/orderActions'
-import { red } from 'colors'
 
 const ProfileScreen = () => {
     const [name, setName] = useState('')
@@ -28,16 +25,12 @@ const ProfileScreen = () => {
     const userUpdateProfile = useSelector(state => state.userUpdateProfile)
     const { success } = userUpdateProfile
 
-    const orderListMy = useSelector(state => state.orderListMy)
-    const { loading:loadingOrders, error:errorOrders, orders } = orderListMy
-
     useEffect(() => {
         if(!userInfo) {
             navigate('/login')
         } else {
             if(!user.name) {
                 dispatch(getUserDetails('profile'))
-                dispatch(listMyOrders())
             } else {
                 setName(user.name)
                 setEmail(user.email)
@@ -96,20 +89,6 @@ const ProfileScreen = () => {
             </Col>
             <Col md={9}>
                 <h2>My Orders</h2>
-                {loadingOrders ? <Loader /> : errorOrders ? <Message variant='danger'>{errorOrders}</Message> : (
-                    <Table striped bordered hover responsive className='table-sm'>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>DATE</th>
-                                <th>TOTAL</th>
-                                <th>PAID</th>
-                                <th>DELIVERED</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                    </Table>
-                )}
             </Col>
         </Row>
     </Container>
