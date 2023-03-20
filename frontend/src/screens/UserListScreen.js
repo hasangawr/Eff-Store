05bf41e3f850e79ae683e5169010390f114a4a48
hasangawr <hasangawr@gmail.com>
2023-03-20 21:33:16 +0530

    Implement user list screen reset & navigation
    Reset user list & navigate to login screen when admin logout

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Button, Table, Container } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -9,13 +10,21 @@ import { listUsers } from '../actions/userActions'
 
 const UserListScreen = () => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     
     const userList = useSelector(state => state.userList)
     const { loading, error, users } = userList
 
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
     useEffect(() => {
-        dispatch(listUsers())
-    }, [dispatch])
+        if(userInfo && userInfo.isAdmin) {
+            dispatch(listUsers())
+        } else {
+            navigate('/login')
+        }
+    }, [dispatch, navigate, userInfo])
 
     const deleteHandler = (id) => {
         console.log('delete')
