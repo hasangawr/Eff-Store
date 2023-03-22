44efea433adc542f2e6770a655db652f541cdf2c
hasangawr <hasangawr@gmail.com>
2023-03-22 18:49:25 +0530

    Implement delete product functionality in frontend

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -6,7 +6,7 @@ import { Button, Table, Container, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts } from '../actions/productActions'
+import { deleteProduct, listProducts } from '../actions/productActions'
 
 const ProductListScreen = () => {
     const dispatch = useDispatch()
@@ -14,6 +14,9 @@ const ProductListScreen = () => {
     
     const productList = useSelector(state => state.productList)
     const { loading, error, products } = productList
+
+    const productDelete = useSelector(state => state.productDelete)
+    const { loading:loadingDelete, error:errorDelete, success:successDelete } = productDelete
 
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
@@ -24,11 +27,11 @@ const ProductListScreen = () => {
         } else {
             navigate('/login')
         }
-    }, [dispatch, navigate, userInfo])
+    }, [dispatch, navigate, userInfo, successDelete])
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure')) {
-            //DELETE PRODUCTS
+            dispatch(deleteProduct(id))
         }
     }
 
@@ -48,6 +51,8 @@ const ProductListScreen = () => {
                 </Button>
             </Col>
         </Row>
+        {loadingDelete && <Loader />}
+        {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
         {loading ? <Loader /> :
          error ? <Message variant='danger'>{error}</Message> :
          (
