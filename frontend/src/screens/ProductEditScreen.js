206fc9ad08a4fff7a7a74c92dfbfce108a00c5f1
hasangawr <hasangawr@gmail.com>
2023-03-24 16:10:16 +0530

    Implemnt frontend image upload functionality

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { Form, Button, Container } from 'react-bootstrap'
@@ -19,6 +20,7 @@ const ProductEditScreen = () => {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
@@ -47,6 +49,29 @@ const ProductEditScreen = () => {
             }
         }
     }, [dispatch, product, productId, successUpdate, navigate])
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append('image', file)
+        setUploading(true)
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Types': 'multipart/form-data'
+                }
+            }
+
+            const { data } = await axios.post('/api/upload', formData, config)
+
+            setImage(data)
+            setUploading(false)
+        } catch (error) {
+            console.error(error)
+            setUploading(false)
+        }
+    }
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -86,6 +111,8 @@ const ProductEditScreen = () => {
                     <Form.Group controlId='image'>
                         <Form.Label class="form-label mt-4">Image</Form.Label>
                         <Form.Control type='text' class="form-control" placeholder='Enter image url' value={image} onChange={(e) => setImage(e.target.value)}></Form.Control>
+                        <Form.Control type='file' id='image-file' label='Choose File' custom onChange={uploadFileHandler}></Form.Control>
+                        {uploading && <Loader />}
                     </Form.Group>
 
                     <Form.Group controlId='brand'>
